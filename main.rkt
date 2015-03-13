15aceb75696db45f51fc41a1f843543d89568621
jeukel <jeukel@gmail.com>
2015-03-13 13:24:12 -0600

    Conditions.

--- main.rkt
+++ main.rkt
@@ -9,6 +9,18 @@
  ; )
 ;)
 (define (main ls)
-(if (null? ls) 'Empty_string
-    (cond (string=? (car ( string-split(ls) )) "ct") (ct (car ( string-split(ls))) (cdr ( string-split(ls))))
+  (if (null? ls) 'Empty_string
+    (cond (string=? (car( string-split(ls) )) "ct")   (ct (car(cdr( string-split(ls)))) (cdr(cdr(string-split(ls)))))
+          (string=? (car( string-split(ls) )) "ins")  (ct (car ( string-split(ls))) (cdr ( string-split(ls))))
+          (string=? (car( string-split(ls) )) "sel")  (ct (car ( string-split(ls))) (cdr ( string-split(ls))))
+          (string=? (car( string-split(ls) )) "act")  (ct (car ( string-split(ls))) (cdr ( string-split(ls))))
+          (string=? (car( string-split(ls) )) "ir")   (ct (car ( string-split(ls))) (cdr ( string-split(ls))))
+          (string=? (car( string-split(ls) )) "boir") (ct (car ( string-split(ls))) (cdr ( string-split(ls))))
+          (string=? (car( string-split(ls) )) "bo")   (ct (car ( string-split(ls))) (cdr ( string-split(ls))))
+          (string=? (car( string-split(ls) )) "elim") (ct (car ( string-split(ls))) (cdr ( string-split(ls))))
+          (string=? (car( string-split(ls) )) "proc") (ct (car ( string-split(ls))) (cdr ( string-split(ls))))
+          (string=? (car( string-split(ls) )) "eval") (ct (car ( string-split(ls))) (cdr ( string-split(ls))))
+    )
+  )
+)
          
