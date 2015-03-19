438ddc5f582a84f151748eb14e63d3f0b49ad7e7
Zyoruk <luisangel1993julio@gmail.com>
2015-03-19 14:33:02 -0600

    trouble with adding a  new line. It is not adding the new line until the execute button in DrRacket is pressed.

--- BaseDeDatos.rkt
+++ BaseDeDatos.rkt
@@ -27,7 +27,6 @@
   );define
 
 (define (ct-aux pWhat pWhereTo)
-  (
    (cond 
      ((empty? pWhat)#f)
      (else (write-to-file (car pWhat) pWhereTo #:mode'text #:exists'append )
@@ -35,13 +34,13 @@
            (ct-aux (cdr pWhat) pWhereTo))
      )
    (wNL pWhereTo)
-   )
   )
 
+;Escribe una nueva linea.
 (define (wNL pWhereTo)
   
   (newline (open-output-file pWhereTo #:mode'text #:exists'append))
-  (close-output-port (open-output-file pWhereTo #:mode'text #:exists'append))
+  ;(close-output-port (open-output-file pWhereTo #:mode'text #:exists'append))
   
   )
 
@@ -55,22 +54,52 @@
         (and (file-exists? pTablaNom)(list? pColList)) 
         (list? pValList))
        (verificadorDeID pTablaNom (car pColList)))
-      (existsID? (cdr(file->list pTablaNom))(car pValList)))(ct-aux pValList pTablaNom)
+      (existsID? (cdr(file->lines pTablaNom))(car pValList)))
+     (ins-aux pTablaNom 
+              (string-split (string-replace(string-replace (string-replace(car (file->lines pTablaNom))"\"" "_") "___" "_")"__""_") "_") pColList pValList 0)
      )
     )
   )
 
-(define (ins-aux pTabla pColList pValList)())
+;Auxiliar function for ins. Makes the recursive part.
+(define (ins-aux pTabla pTablaList pColList pValList pFlag)(cond 
+                                                       ((empty? pTablaList))
+                                                       ((and (empty? pColList) (= 0 pFlag)))
+                                                       ((and (empty? pColList) (= 1 pFlag)) 
+                                                        (write-to-file '"(nil)" pTabla #:mode'text #:exists'append)
+                                                        (write-to-file '_  pTabla #:mode'text #:exists'append )
+                                                        (ins-aux  pTabla  (cdr pTablaList) pColList  pValList 1)
+                                                        )
+                                                       ((equal? (car pTablaList)(car pColList))(cond 
+                                                                                                 ((empty?  pValList) 
+                                                                                                  (write-to-file '"(nil)" pTabla #:mode'text #:exists'append)
+                                                                                                  (write-to-file '_  pTabla #:mode'text #:exists'append )
+                                                                                                  (ins-aux pTabla (cdr pTablaList) (cdr pColList)  pValList 1))
+                                                                                                 (else 
+                                                                                                  (write-to-file (car pValList) pTabla #:mode'text #:exists'append)
+                                                                                                  (write-to-file '_  pTabla #:mode'text #:exists'append )
+                                                                                                  (ins-aux pTabla (cdr pTablaList) (cdr pColList) (cdr pValList)1))
+                                                                                                )
+                                                                                               )
+                                                       ((eq? (equal? (car pTablaList)(car pColList)) #f )
+                                                        (write-to-file '"(nil)" pTabla #:mode'text #:exists'append)
+                                                        (write-to-file '_  pTabla #:mode'text #:exists'append )
+                                                        (ins-aux  pTabla  (cdr pTablaList)  pColList  pValList 1))                                             
+                                                       )
+  (wNL pTabla)
+  )
+
 ;Checks if the unique ID name exists.
 (define (verificadorDeID pTabla pID)
-  (equal? pID (car(string-split (string-replace(string-replace (string-replace(car (file->list pTabla))"\"" "_") "___" "_")"__""_") "_"))))
+  (equal? pID (car(string-split (string-replace(string-replace (string-replace(car (file->lines pTabla))"\"" "_") "___" "_")"__""_") "_"))))
 
 ;Checks existant ID.
 ;receives a list and searches for a existant IDvalue in the first column.
 (define (existsID? list pIDVal)
   (cond
-    ((empty? list) #f)                                    
-    ((equal? pIDVal (car(string-split (string-replace(string-replace (string-replace(car list)"\"" "_") "___" "_")"__""_") "_"))) #t)
+    ((empty? list) #t) 
+    ((empty? (string-split (string-replace(string-replace (string-replace(car list)"\"" "_") "___" "_")"__""_") "_")) #t)
+    ((equal? pIDVal (car(string-split (string-replace(string-replace (string-replace(car list)"\"" "_") "___" "_")"__""_") "_"))) #f)
     (else (existsID? (cdr list) pIDVal))
     ) 
   )
