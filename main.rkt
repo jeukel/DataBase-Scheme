e01cb5f8a8aee4dc3bf7673b40a28fdc22dc17d3
jeukel <jeukel@gmail.com>
2015-03-20 05:46:10 -0600

    Main done.

--- main.rkt
+++ main.rkt
@@ -1,49 +1,71 @@
 #lang racket
 
 (define (main ls)
-      (cond (
-        ((string=? ( car(string-split ls) ) "ct")   (ct (car(cdr( string-split ls ))) (cdr (cdr (string-split ls )))))
-        ((string=? ( car(string-split ls) ) "ins")  (cond( 
-                                                           (list? (cdr(cdr(cdr( string-split ls ))))) (ins (car(cdr( string-split ls ))) ;if & body
-                                                                                                           (car(cdr(cdr( string-split ls )))) 
-                                                                                                           (cdr(cdr(cdr( string-split ls )))) 
-                                                                                                      )
-                                                           (ins( (alt_list( (cdr(cdr(cdr(string-split ls )))) 1 )) ;else
-                                                                 (alt_list( (cdr(cdr(cdr(string-split ls )))) 2 ))
-                                                                )
-                                                           )
-                                                         )
-                                                     ))
-        ((string=? ( car(string-split ls ) ) "sel") (sel (car(cdr( string-split ls ))) ;table name
-                                                         (car(cdr(cdr( string-split ls )))) ;cols o print
-                                                         (cdr(cdr(cdr( string-split ls )))) ;2 values list (col & val)
-                                                     ))
-        ((string=? ( car(string-split ls ) ) "act") (act (car(cdr( string-split ls ))) ;table name
-                                                         (list (alt_list(cdr(cdr(cdr(string-split ls))))) ) ;
-                                                         (list (car(cdr(cdr( string-split ls )))) (alt_list_aux(cdr(cdr(cdr(cdr(string-split ls)))))) )
-                                                     ))
-        ((string=? ( car(string-split ls) ) "boir") (boir (cdr (string-split ls))))
-        ((string=? ( car(string-split ls) ) "ir")   (ir   (cdr (string-split ls))))
-        ((string=? ( car(string-split ls) ) "bo")   (bo   (cdr (string-split ls))))
-        
-            )
+      (cond 
+        [(string=? ( car(string-split ls) ) "ct")   (ct (car(cdr( string-split ls ))) (cdr (cdr (string-split ls ))))]
+        [(string=? ( car(string-split ls) ) "ins")  (cond 
+                                                           [(char=? #\( (string-ref (car(cdr(cdr( string-split ls )))) 0 )) (ins (car(cdr( string-split ls ))) ;if & body
+                                                                                                                                 (append( 
+                                                                                                                                                list ( string-trim (car(cdr(cdr( string-split ls )))) "(" )
+                                                                                                                                                (endof(cdr(cdr(cdr( string-split ls )))) '() )
+                                                                                                                                              ))
+                                                                                                                                 (lastof(cdr(cdr( string-split ls ))))
+                                                                                                                             )]
+                                                           [else ins( (car(cdr( string-split ls )))
+                                                                      null ;columnas
+                                                                      (cdr(cdr( string-split ls ))) ;valores
+                                                                    )
+                                                           ]
+                                                     )]
+        [(string=? ( car(string-split ls ) ) "sel") (cond 
+                                                           [(char=? #\( (string-ref (car(cdr(cdr( string-split ls )))) 0 )) (sel (car(cdr( string-split ls ))) ;if & body
+                                                                                                                                 (append( 
+                                                                                                                                                list ( string-trim (car(cdr(cdr( string-split ls )))) "(" )
+                                                                                                                                                (endof(cdr(cdr(cdr( string-split ls )))) '() )
+                                                                                                                                              ))
+                                                                                                                                 (lastof(cdr(cdr( string-split ls ))))
+                                                                                                                             )]
+                                                           [else sel( (car(cdr( string-split ls )))
+                                                                      null ;columnas
+                                                                      (cdr(cdr( string-split ls ))) ;valores
+                                                                    )
+                                                           ]
+                                                     )]
+        [(string=? ( car(string-split ls ) ) "act") (act (car(cdr( string-split ls ))) ;table name
+                                                         (car(cdr(cdr( string-split ls )))) ;id
+                                                         (append (list (car(cdr(cdr(cdr    (string-split ls))))))  (list (alt_list (cdr(cdr(cdr    (string-split ls))))  '() )) ) ;col names
+                                                         (append (list (car(cdr(cdr(cdr(cdr(string-split ls))))))) (list (alt_list (cdr(cdr(cdr(cdr(string-split ls))))) '() )) ) ;new values
+                                                     )]
+        [(string=? ( car(string-split ls) ) "boir") (boir (cdr (string-split ls)))]
+        [(string=? ( car(string-split ls) ) "ir")   (ir   (cdr (string-split ls)))]
+        [(string=? ( car(string-split ls) ) "bo")   (bo   (cdr (string-split ls)))]
       ) 
 )
 
-(define (alt_list ls n)
-  (cond(
-        ((= n 1) (( alt_list_aux( string-split ls ) )))
-        ((= n 2) (( alt_list_aux( cdr( string-split ls ))) ))
-        ('()) ;"No such option"
-       )
+(define (endof ls apd)
+  (cond 
+    [(char=? #\) (string-ref (car ls) ( - (string-length(car ls)) 1) )) (append apd (string-trim (car ls) ")" )) ]
+    [else (append apd (endof (cdr ls) apd))]
+   )
+)
+
+(define (lastof ls)
+  ( cond
+       [(char=? #\) (string-ref (car ls) ( - (string-length(car ls)) 1) )) (cdr ls)]
+       [else (lastof (cdr ls))]
   )
 )
 
-(define (alt_list_aux ls)
-  (cond(
-        ((integer? (/ (length ls) 2) ) (car (alt_list_aux (cdr(cdr(string-split ls ))))))
-        (car (alt_list_aux (cdr(cdr (string-split ls )))))
-       )
+(define (alt_list ls jmp)
+  (cond
+    [(integer? (/ (length ls) 2) ) (cond
+                                     [(= (length ls) 2) (append jmp (car ls))]
+                                     [else (append jmp (alt_list (cdr(cdr ls )) jmp ))]
+                                   )]
+    [else (cond
+            [(= (length ls) 1) (append jmp (car ls))]
+            [else (append jmp (alt_list (cdr(cdr ls )) jmp ))]
+          )]
   )
 )
 
@@ -52,38 +74,40 @@
 )
   
 (define (test s)
-  (cond (
-         ((= s 1)(obtain_elem_index "act estud 2010002 telefono 5557777 nombre marta" 3))
-         ((= s 2)(main "ins estud 2012001 julio 5554444"))
-         ((= s 3)(main "ct estud carnet nombre telefono"))
-         ((= s 4)(main "ins estud (nombre carnet) maria 2010002"))
-         ((= s 5)(main "sel matricula (carnet nota) siglaCur ce3104"))
-         ((= s 6)(main "act estud 2012001 nombre julio"))
-         ((= s 7)(main "act estud 2010002 telefono 5557777 nombre marta"))
-         ((= s 8)(main "ir matricula carnetEst estud"))
-         ((= s 9)(main "boir matricula carnetEst estud"))
-         ((= s 10)(main "bo estud 2010002"))         
-        )
+  (cond 
+         [(= s 1)(obtain_elem_index "act estud 2010002 telefono 5557777 nombre marta" 3)]
+         [(= s 2)(main "ct estud carnet nombre telefono")]
+         [(= s 3)(main "ins estud 2012001 julio 5554444")]
+         [(= s 4)(main "ins estud (nombre carnet) maria 2010002")]
+         [(= s 5)(main "act estud 2010002 telefono 5557777 nombre marta")]
+         [(= s 6)(main "act estud 2012001 nombre julio")]
+         [(= s 7)(main "sel matricula (carnet nota) siglaCur ce3104")]
+         [(= s 8)(main "ir matricula carnetEst estud")]
+         [(= s 9)(main "boir matricula carnetEst estud")]
+         [(= s 10)(main "bo estud 2010002")]
+         [else '(We're bored)]
   )
 )
 
-(define bo (lambda (x) (+ x 15))
+;(append '(x) '(y))
+
+(define ct (lambda (x y) ( x y ))
   )
 
-(define boir (lambda (x) (+ x 15))
+(define ins (lambda (x y z) (x y z))
   )
 
-(define ir (lambda (x) (+ x 15))
+(define act ( lambda (x y z w) (x y z w) )
   )
 
-(define ct (lambda (x y) (+ x y))
-  ) 
-
-(define ins (lambda (x y) (+ x y))
+(define sel (lambda (x y z) ( x y z ))
   )
 
-(define act (lambda (x y z) (+ (+ x y) z))
+(define ir (lambda (x) (x))
   )
 
-(define sel (lambda (x y z) (+ (+ x y) z))
+(define bo (lambda (x) (x))
+  )
+
+(define boir (lambda (x) (x))
   )
